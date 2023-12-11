c0535f02b2bee773a4068d645f43aeed2751d777
plusSign <plus@vip.qq.com>
2023-12-11 20:19:51 +0800

    import

---
+++ routes/genCard.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+router.param('target', function(req, res, next, t){
+    if (req.user = t)
+    {
+
+    } else {
+      //next(createError(404, 'failed to find user'));
+      req.user = "UNKNOWN";
+    }
+    next();
+  }
+);
+
+/* GET home page. */
+router.get('/:target', function(req, res, next) {
+  res.render('index', { title: "target: " + req.user, name: req.user });
+});
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: "NO target"});
+});
+
+module.exports = router;
